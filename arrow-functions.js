e75bc8ac185a6b6c8284bcd04daa14c402724214
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-06-24 15:27:59 +0000

    feat: :sparkles: add function type explanations

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,38 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum1 = addTwoNumbers(3, 5);
+// console.log(sum);
+
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers(6, 4);
+console.log(sum2);
 
 // Implicit Returns
+const print = message => console.log(message);
+print("Hello, World!");
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
